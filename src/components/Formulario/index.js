0b6f8d385f9b93f2d4364a9d7be5bd4cb46f5876
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-04-20 21:46:59 -0300

    Adiciona formulário de gastos

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,16 +1,41 @@
 import React, { useState } from "react";
+import { Error } from "../Error";
 
 export const Formulario = () => {
+  const [nomeState, setNomeState] = useState("");
+  const [quantidadeState, setQuantidadeState] = useState(0);
+  const [errorState, setErrorState] = useState(false);
+
+  const adicionarDespesa = (e) => {
+    e.preventDefault();
+
+    if (
+      quantidadeState < 1 ||
+      isNaN(quantidadeState) ||
+      nomeState.trim() === ""
+    ) {
+      setErrorState(true);
+      return;
+    }
+
+    setNomeState("");
+    setQuantidadeState(0);
+    setErrorState(false);
+  };
+
   return (
     <div>
-      <form onSubmit={() => {}}>
+      <form onSubmit={adicionarDespesa}>
         <h2>Adicione suas despesas</h2>
+        {errorState && <Error>Erro no formulário</Error>}
         <div className="campo">
           <label htmlFor="despesa">Nome da despesa</label>
           <input
             id="despesa"
             className="u-full-width"
             placeholder="exemplo Aluguel"
+            value={nomeState}
+            onChange={(e) => setNomeState(e.target.value)}
             type="text"
           />
 
@@ -19,6 +44,8 @@ export const Formulario = () => {
             id="quantidade"
             className="u-full-width"
             placeholder="exemplo 1"
+            value={quantidadeState}
+            onChange={(e) => setQuantidadeState(parseInt(e.target.value))}
             type="number"
           />
 
